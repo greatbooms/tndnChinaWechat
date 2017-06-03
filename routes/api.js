10fda30f4bda30a520334d8417c7555f07087628
SD-HOME-PC\SD-HOME <greatbooms@tndn.net>
2017-06-04 03:37:02 +0900

    유저 주소 입력 수정 삭제 완성

--- routes/api.js
+++ routes/api.js
@@ -53,8 +53,13 @@ router.get('/getGoodsList', function(req, res, next) {
   databasePool.getConnection(function(err, connection) {
     connection.query(queryStr, function(error, rows, fields) {
       connection.release();
-      res.contentType('application/json; charset=utf-8');
-      res.end(JSON.stringify(rows));
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({}));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify(rows));
+      }
     });
   });
 });
@@ -142,10 +147,10 @@ router.post('/setCartItem', function(req, res, next) {
       connection.release();
       if (error) {
         res.contentType('application/json; charset=utf-8');
-        res.end(JSON.stringify({result: "FAIL"}));
+        res.end(JSON.stringify({ result: "FAIL" }));
       } else {
         res.contentType('application/json; charset=utf-8');
-        res.end(JSON.stringify({result: "SUCCESS"}));
+        res.end(JSON.stringify({ result: "SUCCESS" }));
       }
     });
   });
@@ -168,8 +173,13 @@ router.get('/getCartItem', function(req, res, next) {
   databasePool.getConnection(function(err, connection) {
     connection.query(queryStr, function(error, rows, fields) {
       connection.release();
-      res.contentType('application/json; charset=utf-8');
-      res.end(JSON.stringify(rows));
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({}));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify(rows));
+      }
     });
   });
 });
@@ -179,9 +189,11 @@ router.put('/updateCartItem', function(req, res, next) {
   var quantity = req.body.quantity;
 
   // console.log(req);
-  console.log(quantity);
+  console.log('cartId = ' + cartId);
+  console.log('quantity = ' + quantity);
   var queryStr = 'UPDATE cart ';
   queryStr += 'SET quantity = ' + quantity + ', ';
+  queryStr += 'status_flag = 2, ';
   queryStr += 'update_date = NOW() ';
   queryStr += 'WHERE id = ' + cartId;
 
@@ -191,10 +203,10 @@ router.put('/updateCartItem', function(req, res, next) {
       connection.release();
       if (error) {
         res.contentType('application/json; charset=utf-8');
-        res.end(JSON.stringify({result: "FAIL"}));
+        res.end(JSON.stringify({ result: "FAIL" }));
       } else {
         res.contentType('application/json; charset=utf-8');
-        res.end(JSON.stringify({result: "SUCCESS"}));
+        res.end(JSON.stringify({ result: "SUCCESS" }));
       }
     });
   });
@@ -202,6 +214,8 @@ router.put('/updateCartItem', function(req, res, next) {
 
 router.delete('/deleteCartItem', function(req, res, next) {
   var cartId = req.body.idx_cart;
+
+  console.log('cartId = ' + cartId);
 
   var queryStr = 'UPDATE cart ';
   queryStr += 'SET status_flag = 3, ';
@@ -214,10 +228,333 @@ router.delete('/deleteCartItem', function(req, res, next) {
       connection.release();
       if (error) {
         res.contentType('application/json; charset=utf-8');
-        res.end(JSON.stringify({result: "FAIL"}));
+        res.end(JSON.stringify({ result: "FAIL" }));
       } else {
         res.contentType('application/json; charset=utf-8');
-        res.end(JSON.stringify({result: "SUCCESS"}));
+        res.end(JSON.stringify({ result: "SUCCESS" }));
+      }
+    });
+  });
+});
+
+router.get('/getAreacode', function(req, res, next) {
+  var topno = req.query.topno;
+
+  console.log('topno = ' + topno);
+
+  var queryStr = 'SELECT ';
+  queryStr += 'id, ';
+  queryStr += 'areaname, ';
+  queryStr += 'no, ';
+  queryStr += 'topno, ';
+  queryStr += 'areacode, ';
+  queryStr += 'arealevel, ';
+  queryStr += 'typename ';
+  queryStr += 'FROM quan_prov_city_area ';
+  queryStr += 'WHERE topno = ' + topno + ' ';
+  queryStr += 'ORDER BY no';
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({}));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify(rows));
+      }
+    });
+  });
+});
+
+router.post('/setUserAddress', function(req, res, next) {
+  var idx_user = req.body.idx_user;
+  var weixin_id = req.body.weixin_id;
+  var name = req.body.name;
+  var sex = req.body.sex;
+  var contacts = req.body.contacts;
+  var zip_code = req.body.zip_code;
+  var address_1 = req.body.address_1;
+  var address_2 = req.body.address_2;
+  var address_3 = req.body.address_3;
+  var address_detail = req.body.address_detail;
+
+  console.log('idx_user = ' + idx_user);
+  console.log('weixin_id = ' + weixin_id);
+  console.log('name = ' + name);
+  console.log('sex = ' + sex);
+  console.log('contacts = ' + contacts);
+  console.log('zip_code = ' + zip_code);
+  console.log('address_1 = ' + address_1);
+  console.log('address_2 = ' + address_2);
+  console.log('address_3 = ' + address_3);
+  console.log('address_detail = ' + address_detail);
+
+  if (idx_user == undefined || idx_user == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "idx_user FAIL" }));
+    return;
+  } else if (weixin_id == undefined || weixin_id == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "weixin_id FAIL" }));
+    return;
+  }
+
+  var queryStr = 'INSERT INTO user_address(';
+  queryStr += 'idx_user, ';
+  queryStr += 'weixin_id, ';
+  queryStr += 'name, ';
+  queryStr += 'sex, ';
+  queryStr += 'contacts, ';
+  queryStr += 'zip_code, ';
+  queryStr += 'address_1, ';
+  queryStr += 'address_2, ';
+  queryStr += 'address_3, ';
+  queryStr += 'address_detail, ';
+  queryStr += 'status_flag, ';
+  queryStr += 'insert_date) ';
+  queryStr += 'VALUES(';
+  queryStr += idx_user + ', ';
+  queryStr += '"' + weixin_id + '", ';
+  queryStr += '"' + name + '", ';
+  queryStr += '"' + sex + '", ';
+  queryStr += '"' + contacts + '", ';
+  queryStr += '"' + zip_code + '", ';
+  queryStr += '"' + address_1 + '", ';
+  queryStr += '"' + address_2 + '", ';
+  queryStr += '"' + address_3 + '", ';
+  queryStr += '"' + address_detail + '", ';
+  queryStr += '1, ';
+  queryStr += 'NOW())';
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({ result: "FAIL" }));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({ result: "SUCCESS" }));
+      }
+    });
+  });
+});
+
+router.get('/getUserAddressList', function(req, res, next) {
+  var idx_user = req.query.idx_user;
+
+  console.log('idx_user = ' + idx_user);
+
+  if (idx_user == undefined || idx_user == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "idx_user FAIL" }));
+    return;
+  }
+
+  var queryStr = 'SELECT ';
+  queryStr += 'id, ';
+  queryStr += 'weixin_id, ';
+  queryStr += 'name, ';
+  queryStr += 'sex, ';
+  queryStr += 'contacts, ';
+  queryStr += 'zip_code, ';
+  queryStr += '(SELECT areaname FROM quan_prov_city_area where no = address_1) as addressName_1, ';
+  queryStr += '(SELECT areaname FROM quan_prov_city_area where no = address_2) as addressName_2, ';
+  queryStr += '(SELECT areaname FROM quan_prov_city_area where no = address_3) as addressName_3, ';
+  queryStr += 'address_detail ';
+  queryStr += 'FROM user_address ';
+  queryStr += 'WHERE status_flag != 3 ';
+  queryStr += 'AND idx_user = ' + idx_user + ' ';
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({}));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify(rows));
+      }
+    });
+  });
+});
+
+router.get('/getUserAddressList', function(req, res, next) {
+  var idx_user = req.query.idx_user;
+
+  console.log('idx_user = ' + idx_user);
+
+  if (idx_user == undefined || idx_user == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "idx_user FAIL" }));
+    return;
+  }
+
+  var queryStr = 'SELECT ';
+  queryStr += 'id, ';
+  queryStr += 'weixin_id, ';
+  queryStr += 'name, ';
+  queryStr += 'sex, ';
+  queryStr += 'contacts, ';
+  queryStr += 'zip_code, ';
+  queryStr += '(SELECT areaname FROM quan_prov_city_area where no = address_1) as addressName_1, ';
+  queryStr += '(SELECT areaname FROM quan_prov_city_area where no = address_2) as addressName_2, ';
+  queryStr += '(SELECT areaname FROM quan_prov_city_area where no = address_3) as addressName_3, ';
+  queryStr += 'address_detail ';
+  queryStr += 'FROM user_address ';
+  queryStr += 'WHERE status_flag != 3 ';
+  queryStr += 'AND idx_user = ' + idx_user + ' ';
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({}));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify(rows));
+      }
+    });
+  });
+});
+
+router.get('/getUserAddressDetail', function(req, res, next) {
+  var idx_address = req.query.idx_address;
+
+  console.log('idx_address = ' + idx_address);
+
+  if (idx_address == undefined || idx_address == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "idx_address FAIL" }));
+    return;
+  }
+
+  var queryStr = 'SELECT ';
+  queryStr += 'id, ';
+  queryStr += 'weixin_id, ';
+  queryStr += 'name, ';
+  queryStr += 'sex, ';
+  queryStr += 'contacts, ';
+  queryStr += 'zip_code, ';
+  queryStr += 'address_1, ';
+  queryStr += 'address_2, ';
+  queryStr += 'address_3, ';
+  queryStr += 'address_detail ';
+  queryStr += 'FROM user_address ';
+  queryStr += 'WHERE status_flag != 3 ';
+  queryStr += 'AND id = ' + idx_address + ' ';
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({}));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify(rows));
+      }
+    });
+  });
+});
+
+router.put('/updateUserAddress', function(req, res, next) {
+  var idx_address = req.body.idx_address;
+  var weixin_id = req.body.weixin_id;
+  var name = req.body.name;
+  var sex = req.body.sex;
+  var contacts = req.body.contacts;
+  var zip_code = req.body.zip_code;
+  var address_1 = req.body.address_1;
+  var address_2 = req.body.address_2;
+  var address_3 = req.body.address_3;
+  var address_detail = req.body.address_detail;
+
+  console.log('idx_address = ' + idx_address);
+  console.log('weixin_id = ' + weixin_id);
+  console.log('name = ' + name);
+  console.log('sex = ' + sex);
+  console.log('contacts = ' + contacts);
+  console.log('zip_code = ' + zip_code);
+  console.log('address_1 = ' + address_1);
+  console.log('address_2 = ' + address_2);
+  console.log('address_3 = ' + address_3);
+  console.log('address_detail = ' + address_detail);
+
+  if (idx_address == undefined || idx_address == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "idx_address FAIL" }));
+    return;
+  } else if (weixin_id == undefined || weixin_id == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "weixin_id FAIL" }));
+    return;
+  }
+
+  var queryStr = 'UPDATE user_address ';
+  queryStr += 'SET weixin_id = "' + weixin_id + '", ';
+  queryStr += 'name = "' + name + '", ';
+  queryStr += 'sex = "' + sex + '", ';
+  queryStr += 'contacts = "' + contacts + '", ';
+  queryStr += 'zip_code = "' + zip_code + '", ';
+  queryStr += 'address_1 = "' + address_1 + '", ';
+  queryStr += 'address_2 = "' + address_2 + '", ';
+  queryStr += 'address_3 = "' + address_3 + '", ';
+  queryStr += 'address_detail = "' + address_detail + '", ';
+  queryStr += 'status_flag = 2, ';
+  queryStr += 'update_date = NOW() ';
+  queryStr += 'WHERE id = ' + idx_address;
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({ result: "FAIL" }));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({ result: "SUCCESS" }));
+      }
+    });
+  });
+});
+
+router.delete('/deleteUserAddress', function(req, res, next) {
+  var idx_address = req.body.idx_address;
+
+  console.log('idx_address = ' + idx_address);
+
+  if (idx_address == undefined || idx_address == '') {
+    res.contentType('application/json; charset=utf-8');
+    res.end(JSON.stringify({ result: "idx_address FAIL" }));
+    return;
+  }
+
+  var queryStr = 'UPDATE user_address ';
+  queryStr += 'SET status_flag = 3, ';
+  queryStr += 'update_date = NOW() ';
+  queryStr += 'WHERE id = ' + idx_address;
+
+  console.log(queryStr);
+  databasePool.getConnection(function(err, connection) {
+    connection.query(queryStr, function(error, rows, fields) {
+      connection.release();
+      if (error) {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({ result: "FAIL" }));
+      } else {
+        res.contentType('application/json; charset=utf-8');
+        res.end(JSON.stringify({ result: "SUCCESS" }));
       }
     });
   });
